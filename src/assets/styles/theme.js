0b0fe06bc35295bb0b9fe0ac060e143126dc2a91
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-01-26 22:31:28 +0200

    feat: theme

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,1 +1,38 @@
-export const theme = {};
+import { device } from './mixins';
+
+export const theme = Object.freeze({
+  colors: {
+    white: '#ffffff',
+    black: '000000',
+    grey: '#BDBDBD',
+    accentBlue: '#4A56E2',
+    accentGreen: '#24CCA7',
+    accentPink: '#FF6596',
+    background: '#E7EAF2',
+  },
+  fonts: {
+    primary: 'Circe, sans serif',
+    secondary: 'Poppins, sans serif',
+  },
+  fontSizes: {
+    xs: '12px',
+    sm: '16px',
+    md: '18px',
+    lg: '24px',
+    title: '30px',
+  },
+  fontWeight: {
+    normal: 400,
+    bold: 700,
+  },
+  spacing: value => `${4 * value}px`,
+  animation: {
+    cubicBezier: '0.25s cubic-bezier(0.7, 0.98, 0.86, 0.98)',
+  },
+  media: {
+    mobile: `@media screen and ${device.mobile}`,
+    tablet: `@media screen and ${device.tablet}`,
+    desktop: `@media screen and ${device.desktop}`,
+    mobileOnly: `@media screen and ${device.mobileOnly}`,
+  },
+});
