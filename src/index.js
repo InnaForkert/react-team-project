3b3bbf58a7a112151816addf65b241ae3d1db763
Inna <inna.forkert@gmail.com>
2023-01-26 13:21:51 +0200

    add: basic project structure
    add:
    - redux, router, styled components, notiflix, chart.js npm packages;
    - redux store structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'assets/styles/theme';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter
+        // basename="react-team-project"
+        >
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,32 @@
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const persistConfig = {
+  key: 'user',
+  storage,
+  whitelist: ['token'],
+};
+
+const rootReducer = combineReducers({});
+
+export const store = configureStore({
+  rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
