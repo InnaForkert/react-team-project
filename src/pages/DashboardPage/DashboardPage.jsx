85da7e451e69093a9c3e5cc85bed9f2ae5c48e89
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-01-26 19:43:56 +0200

    feat: navigation

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,19 +1,15 @@
 import { Outlet } from 'react-router';
-import { Link } from 'react-router-dom';
 import { Suspense } from 'react';
 import HomeTab from 'components/HomeTab/HomeTab';
 import Loader from 'components/Loader/Loader';
+import Navigation from 'components/Navigation/Navigation';
 
 export default function DashboardPage() {
   return (
     <>
       <h1>DashboardPage</h1>
       <HomeTab />
-      <Link to="home">home</Link>
-      <br />
-      <Link to="diagram">diagram</Link>
-      <br />
-      <Link to="currency">currency for mobile version</Link>
+      <Navigation />
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
