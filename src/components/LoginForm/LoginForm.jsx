bdb438f89a25b1a79f5ee48fcbb98b7bcd735bec
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-01-27 07:57:46 +0200

    init: add Link to register and login forms

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import { signIn } from 'redux/auth/operations';
 
 import css from './LoginForm.module.css';
@@ -22,31 +24,37 @@ export const LoginForm = () => {
   const handleSubmit = e => {
     e.preventDefault();
     dispatch(signIn({ email, password }));
+    setAuthData(INITIAL_STATE);
   };
 
   return (
-    <form className={css.loginForm} onSubmit={handleSubmit}>
-      <label>
-        E-mail
-        <input
-          type="email"
-          name="email"
-          onChange={handleChange}
-          value={email}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          name="password"
-          onChange={handleChange}
-          value={password}
-          required
-        />
-      </label>
-      <button type="submit" /*disabled={isSubmitting}*/>Log in</button>
-    </form>
+    <div className={css.loginWrapper}>
+      <form className={css.loginForm} onSubmit={handleSubmit}>
+        <label>
+          E-mail
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            value={email}
+            required
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={password}
+            minLength="6"
+            maxLength="12"
+            required
+          />
+        </label>
+        <button type="submit" /*disabled={isSubmitting}*/>Log in</button>
+      </form>
+      <Link to="/register">Register</Link>
+    </div>
   );
 };
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { signUp } from 'redux/auth/operations';
+import { Link } from 'react-router-dom';
 
 import css from './RegistrationForm.module.css';
 
@@ -24,51 +25,61 @@ export const RegistrationForm = () => {
   const handleSubmit = e => {
     e.preventDefault();
     dispatch(signUp({ email, password, username }));
+    setAuthData(INITIAL_STATE);
   };
 
   return (
-    <form className={css.registrationForm} onSubmit={handleSubmit}>
-      <label>
-        E-mail
-        <input
-          type="email"
-          name="email"
-          onChange={handleChange}
-          value={email}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          name="password"
-          onChange={handleChange}
-          value={password}
-          required
-        />
-      </label>
-      <label>
-        Confirm password
-        <input
-          type="password"
-          name="confirmPassword"
-          onChange={handleChange}
-          value={confirmPassword}
-          required
-        />
-      </label>
-      <label>
-        First name
-        <input
-          type="text"
-          name="username"
-          onChange={handleChange}
-          value={username}
-          required
-        />
-      </label>
-      <button type="submit" /*disabled={isSubmitting}*/>Register</button>
-    </form>
+    <div className={css.registrationWrapper}>
+      <form className={css.registrationForm} onSubmit={handleSubmit}>
+        <label>
+          E-mail
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            value={email}
+            required
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={password}
+            minLength="6"
+            maxLength="12"
+            required
+          />
+        </label>
+        <label>
+          Confirm password
+          <input
+            type="password"
+            name="confirmPassword"
+            onChange={handleChange}
+            value={confirmPassword}
+            minLength="6"
+            maxLength="12"
+            required
+          />
+        </label>
+        <label>
+          First name
+          <input
+            type="text"
+            name="username"
+            onChange={handleChange}
+            value={username}
+            minLength="1"
+            maxLength="12"
+            required
+          />
+        </label>
+        <button type="submit" /*disabled={isSubmitting}*/>Register</button>
+      </form>
+      <Link to="/login">Log in</Link>
+    </div>
   );
 };
