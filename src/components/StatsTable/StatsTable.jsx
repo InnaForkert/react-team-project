3c81c35466aab1a433bc2904e2860c4edc792c24
Inna <inna.forkert@gmail.com>
2023-01-28 18:24:15 +0200

    feat: custom dropdown

--- src/components/StatsTable/StatsTable.jsx
+++ src/components/StatsTable/StatsTable.jsx
@@ -1,56 +1,89 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTransactionsSummary } from 'redux/transactions/operations';
-import { SelectDate } from './StatsTable.styled';
+import { SelectDate, SelectInput, SelectContainer } from './StatsTable.styled';
 import { selectSummary } from 'redux/transactions/transactionsSlice';
 import { nanoid } from 'nanoid';
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
 function StatsTable() {
   const dispatch = useDispatch();
   const summary = useSelector(selectSummary);
-
-  useEffect(() => {
-    dispatch(fetchTransactionsSummary());
-  }, [dispatch]);
+  const [monthDropdownShown, setMonthDropdownShown] = useState(false);
+  const [yearDropdownShown, setYearDropdownShown] = useState(false);
 
   const now = new Date();
 
   const [month, setMonth] = useState(now.getMonth());
   const [year, setYear] = useState(now.getFullYear());
 
+  useEffect(() => {
+    dispatch(fetchTransactionsSummary({ month: +month + 1, year }));
+  }, [dispatch, month, year]);
+
   function handleMonthChange(e) {
-    setMonth(e.value);
-    dispatch(fetchTransactionsSummary({ month, year }));
+    setMonth(e.target.dataset.value);
+    toggleMonthDropdown();
   }
 
   function handleYearChange(e) {
-    setYear(e.value);
-    dispatch(fetchTransactionsSummary({ month, year }));
+    setYear(e.target.dataset.value);
+    toggleYearDropdown();
+  }
+
+  function toggleMonthDropdown() {
+    setMonthDropdownShown(state => !state);
+    setYearDropdownShown(false);
+  }
+
+  function toggleYearDropdown() {
+    setYearDropdownShown(state => !state);
+    setMonthDropdownShown(false);
   }
 
   return (
     <>
-      <SelectDate name="month" value={month} onChange={handleMonthChange}>
-        <option value="0">January</option>
-        <option value="1">February</option>
-        <option value="2">March</option>
-        <option value="3">April</option>
-        <option value="4">May</option>
-        <option value="5">June</option>
-        <option value="6">July</option>
-        <option value="7">August</option>
-        <option value="8">September</option>
-        <option value="9">October</option>
-        <option value="10">November</option>
-        <option value="11">December</option>
-      </SelectDate>
-      <SelectDate name="year" value={year} onChange={handleYearChange}>
-        <option value="2019">2019</option>
-        <option value="2020">2020</option>
-        <option value="2021">2021</option>
-        <option value="2022">2022</option>
-        <option value="2023">2023</option>
-      </SelectDate>
+      <SelectContainer className={monthDropdownShown ? 'dropdownShown' : ''}>
+        <SelectInput onClick={toggleMonthDropdown}>{months[month]}</SelectInput>
+        <SelectDate name="month" onClick={handleMonthChange}>
+          <li data-value="0">January</li>
+          <li data-value="1">February</li>
+          <li data-value="2">March</li>
+          <li data-value="3">April</li>
+          <li data-value="4">May</li>
+          <li data-value="5">June</li>
+          <li data-value="6">July</li>
+          <li data-value="7">August</li>
+          <li data-value="8">September</li>
+          <li data-value="9">October</li>
+          <li data-value="10">November</li>
+          <li data-value="11">December</li>
+        </SelectDate>
+      </SelectContainer>
+      <SelectContainer className={yearDropdownShown ? 'dropdownShown' : ''}>
+        <SelectInput onClick={toggleYearDropdown}>{year}</SelectInput>
+        <SelectDate name="year" onClick={handleYearChange}>
+          <li data-value="2019">2019</li>
+          <li data-value="2020">2020</li>
+          <li data-value="2021">2021</li>
+          <li data-value="2022">2022</li>
+          <li data-value="2023">2023</li>
+        </SelectDate>
+      </SelectContainer>
       {/* <TableHeader>Category, Sum</TableHeader> */}
       <ul>
         {summary.length > 0
