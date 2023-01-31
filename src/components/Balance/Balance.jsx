4badc454d0f4c2c6fd502107c270da98df67aea9
Inna <inna.forkert@gmail.com>
2023-02-01 00:09:15 +0200

    add: hometab

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,7 +1,17 @@
-import { Title } from "./Balance.styled";
+import { useSelector } from 'react-redux';
+import { BalanceContainer, BalanceText, LightText } from './Balance.styled';
+import { formatMoney } from 'utils/formatMoney';
 
 export default function Balance() {
+  const balance = useSelector(state => state.auth.user.balance);
+
   return (
-    <Title>Balance</Title>
-  )
+    // <Title>Balance</Title>
+    <BalanceContainer>
+      Your balance
+      <BalanceText>
+        <LightText>₴</LightText> {formatMoney(balance)}
+      </BalanceText>
+    </BalanceContainer>
+  );
 }
